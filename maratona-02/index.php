9e678a17a6f8dd85fccc81cf5b75f82f89aac4fe
GUSTAVO ANTONIO <gustavo.antonio@senacsp.edu.br>
2025-12-03 17:03:30 -0300

    Atualização

--- maratona-02/index.php
+++ maratona-02/index.php
@@ -7,7 +7,25 @@
 </head>
 <body>
     <?php 
+    echo "2. Atividade classificação de nota de um aluno";
+    echo "<br>";
     
+$notaaluno = 9;
+  if($notaaluno >= 10){
+        echo"Voce foi aprovado com distinção.";
+     }   elseif($notaaluno >= 0 && $notaaluno <= 4.9){
+        echo "voce esta reprovado";
+    }else if($notaaluno >= 5 && $notaaluno <= 6.9){
+        echo "voce esta em recuperação.";
+    }else if($notaaluno >= 7 && $notaaluno <= 8.9){
+        echo " voce foi aprovado";
+ }else if ($notaaluno >= 9 && $notaaluno <= 10){
+        echo "Aprovado";
+    }
+    echo "<br>";
+    echo "Atividade 4. Verificando se um numero é par ou impar.";
+    
+
     ?>
 </body>
 </html>
